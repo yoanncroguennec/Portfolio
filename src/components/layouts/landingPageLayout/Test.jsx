29f69fbab1608e6b15511fa60ba2044c18509e25
sido69 <yoann.croguennec@gmail.com>
2023-08-31 19:53:06 +0200

    first commit

--- src/components/layouts/landingPageLayout/Test.jsx
+++ src/components/layouts/landingPageLayout/Test.jsx
@@ -78,9 +78,58 @@ export default function Test() {
   };
   
   return (
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
-      voluptas?
+    <div id='technologies'>
+      <Image
+        src='/assets/imgs/personnage2D/personnage.png'
+        alt='personnage2D Contact'
+        height={matches ? "150" : "300"}
+        width={matches ? "150" : "300"}
+        style={{ marginBottom: "-40px" }}
+      />
+
+      <BoxCard
+        data-aos='fade-up'
+        data-aos-easing='ease-out-cubic'
+        data-aos-duration='2000'
+      >
+        <Image
+          alt='techs'
+          src={img}
+          height={matches ? "80" : "120"}
+          width={matches ? "300" : "400"}
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5'>
+            {title}
+          </Typography>
+          {data.map((item, index) => {
+            return (
+              <BoxContent key={index}>
+                <Image
+                  alt='icon'
+                  src={item.icon}
+                  height={30}
+                  width={30}
+                  style={{ marginRight: "25px" }}
+                />
+                <Typography>{item.name}</Typography>
+              </BoxContent>
+            );
+          })}
+        </CardContent>
+        <CardActionsBox>
+          <FaChevronLeft
+            onClick={prevPerson}
+            size={40}
+            style={{ cursor: "pointer" }}
+          />
+          <FaChevronRight
+            onClick={nextPerson}
+            size={40}
+            style={{ cursor: "pointer" }}
+          />
+        </CardActionsBox>
+      </BoxCard>
     </div>
   );
 }
